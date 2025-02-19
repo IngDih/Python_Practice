f4944beda0db4fb78effd9641685e7d286f2cff2
IngDih <ingrid.meccas@gmail.com>
2025-02-19 13:52:59 -0800

    Creation of ch7 and Cheat Sheet

---
+++ Chapter_7.py
@@ -0,0 +1,14 @@
+# Chapter 7 #
+
+# USER INPUT AND WHILE LOOPS #
+
+#SYNTAX 
+
+#Methods
+    # .input() - This function pauses the program and waits for user input. This input will be assigned to a variable.
+
+
+#Examples
+name = input("Please enter your name: ")
+print(f"My name is {name}.")
+print(f"Hello {name}!")
---
+++ Python_Cheat_Sheet.py
@@ -0,0 +1,16 @@
+"""
+Escape Sequence	Meaning
+\n	Newline (moves to the next line)
+\t	Tab (adds a horizontal tab space)
+\r	Carriage return (moves cursor to the beginning of the line)
+\b	Backspace (deletes the previous character)
+\f	Form feed (used in old printers, moves to a new page)
+\v	Vertical tab (moves down but not to a new line)
+\\	Prints a backslash \
+\'	Prints a single quote '
+\"	Prints a double quote "
+\xhh	Prints a character with hex value hh (e.g., \x41 → A)
+\uXXXX	Unicode character (e.g., \u2764 → ❤️)
+"""
+
+
