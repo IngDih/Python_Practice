dcd427ef54d2147500d9b0fdaf3f0fa0a0068292
IngDih <ingrid.meccas@gmail.com>
2025-02-19 18:38:12 -0800

    Ch7 While loops

--- Chapter_7.py
+++ Chapter_7.py
@@ -3,7 +3,7 @@
 
 # USER INPUT AND WHILE LOOPS #
 
-#SYNTAX 
+#INPUT SYNTAX 
 
 #Methods
     # .input() - This function pauses the program and waits for user input. This input will be assigned to a variable.
@@ -47,11 +47,122 @@ print("------------------")
 
 
 #7-3
-number = int(input("Tell me a number! \n"))
+# number = int(input("Tell me a number! \n"))
 
-if number % 10 == 0:
-    print("This number is divisible by 10.")
-else:
-    print("This number is not divisible by 10.")
+# if number % 10 == 0:
+#     print("This number is divisible by 10.")
+# else:
+#     print("This number is not divisible by 10.")
+# print("------------------")
+
+
+#While Loops
+
+#Trying out
+# prompt = "Hey, what are your favorite foods?\n"
+# prompt += "(type 'quit' to exit loop)\n"
+
+# fave_foods = []  # Initialize an empty list to store foods
+
+# while True:
+#     food = input(prompt)
     
+#     if food.lower() == 'quit':  
+#         if len(fave_foods) > 1:
+#             food_list = ', '.join(fave_foods[:-1]) + ' and ' + fave_foods[-1]
+#         else:
+#             food_list = fave_foods[0]
 
+#         print(f"You like {food_list}. Thank you for your answers, bye!")
+#         break
+#     else:
+#         fave_foods.append(food)
+# print("------------------")
+
+
+#7-4
+# prompt = "Enter desired pizza toppings:\n"
+
+# while True:
+#     toppings = input(prompt)
+    
+#     if toppings == 'quit':
+#         print("Bye!")
+#         break
+#     else:
+#         print(f"We're adding {toppings} to your pizza!")
+# print("------------------")
+
+
+#7-5
+# message = "Enter your age:\n"
+# print("Welcome to the Royal Cinema!\nType 'quit' to exit.")
+
+# while True:
+#     age = input(message)
+    
+#     if age == "quit":
+#         print("Thanks for choosing the Royal Cinema, bye!")
+#         break
+#     elif int(age) < 3:
+#         print("This ticket is on us! Enjoy a free ticket!")
+#     elif int(age) <= 12:
+#         print("Your ticket costs US$ 10.")
+#     elif int(age) > 12:
+#         print("Your ticket costs US$ 15.")
+ 
+# print("------------------")
+
+
+#7-6
+#Since those were done above, I'm giving a try on a for loop inside a while loop (for range).
+# message = "Enter your age:\n"
+# print("Welcome to the Royal Cinema!\n")
+
+# while True:
+#     people = input("How many people are you buying tickets for?\n")
+    
+#     if not people.isdigit() or int(people) <= 0:  
+#         print("Please enter a valid number of people (greater than 0).")
+#         continue
+
+#     people = int(people)  
+    
+#     for i in range(people):
+#         age = input(f"Person {i + 1}, {message}")  # i = 0 on first iteration
+        
+#         if age.lower() == "quit":  
+#             print("Thanks for choosing the Royal Cinema, bye!")
+#             exit()  
+
+#         if not age.isdigit():  
+#             print("Invalid input. Please enter a number for age.")
+#             continue  
+
+#         age = int(age)
+
+#         if age < 3:
+#             print("This ticket is on us! Enjoy a free ticket!")
+#         elif age <= 12:
+#             print("Your ticket costs US$ 10.")
+#         else:
+#             print("Your ticket costs US$ 15.")
+
+#     # Ask if they want to buy more tickets
+#     more = input("Do you want to buy more tickets? (yes/no)\n").strip().lower()
+#     if more != "yes":
+#         print("Thanks for choosing the Royal Cinema, bye!")
+#         break  
+# print("------------------")
+ 
+
+#7-7
+#This is an infinity loop, ctrl + C to stop running.
+name = input("Type your name:\n")
+
+while True:
+    print(f"I love you {name}.")
+    
+    
+         
+    
